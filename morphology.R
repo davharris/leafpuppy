501e108b4cd8dc19ac1c6d7eb0514b4c5fd71072
davharris <harry491@gmail.com>
2014-04-20 19:06:21 -0700

    morphology.R

---
+++ morphology.R
@@ -0,0 +1,35 @@
+library("EBImage")
+library(png)
+img = readPNG("images/wMTjH3L.png")
+kern = makeBrush(5, shape = "disc")
+
+processed.image = closing(
+  openingGreyScale(gblur(img, radius = 3, sigma = 1.5), kern),
+  kern
+)
+
+imageplot = function(x, ...){
+  image(x, ..., col = gray(seq(0, 1, length = 12)), asp = 1, axes = FALSE)
+}
+
+
+# Based on procedure from ?EBImage::thresh
+w=5;h=5;offset = -.01
+x = processed.image
+f = matrix(1, nc=2*w+1, nr=2*h+1) ; f=f/sum(f) ; 
+soft.thresh = x-filter2(x, f)+offset
+
+png("morphology.png", width = 2048, height = 2048)
+par(mfrow = c(2, 2))
+par(bty = "n")
+imageplot(img, main = "original")
+imageplot(processed.image, main = "processed")
+imageplot(soft.thresh, main = "soft threshold")
+imageplot(
+  thresh(
+    processed.image,
+    offset = offset
+  ),
+  main = "hard threshold"
+)
+dev.off()
